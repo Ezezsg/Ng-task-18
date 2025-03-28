fc40702b4f258e5ff9677d1415ab1a1e9c87b9f5
Ezezsg <ezequielfperez@gmail.com>
2025-03-28 14:17:29 -0300

    listar tareas y presentación de botón edit

--- src/app/task/data-access/task.service.ts
+++ src/app/task/data-access/task.service.ts
@@ -1,5 +1,7 @@
-import { inject, Injectable } from '@angular/core';
-import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { inject, Injectable, signal } from '@angular/core';
+import { Firestore, collection, addDoc, collectionData  } from '@angular/fire/firestore';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, Observable, throwError, tap } from 'rxjs';
 
 export interface Task {
   id: string;
@@ -18,7 +20,25 @@ const PATH = 'tasks';
 export class TaskService {
   private _firestore = inject(Firestore);
   private _collection = collection(this._firestore, PATH)
+  loading = signal<boolean>(true);
   
+  getTasks = toSignal(
+    (
+      collectionData(this._collection, {idField: 'id'}) as Observable<Task[]>
+    ).pipe(
+      tap(() => {
+        this.loading.set(false);
+      }),
+      catchError(error =>{
+        this.loading.set(false);
+        return throwError(() => error);
+      })
+    ), 
+      {
+        initialValue: [],
+      }
+  );
+
   create(task: TaskCreate) {
     return addDoc(this._collection, task);
   }
--- src/app/task/ui/table/table.component.ts
+++ src/app/task/ui/table/table.component.ts
@@ -1,14 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input } from '@angular/core';
 import { Task } from '../../data-access/task.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-table',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './table.component.html',
-  
 })
 export class TableComponent {
 
   tasks = input.required<Task[]>();
 
+  // constructor() {
+  //   effect(() => {
+  //     console.log(this.tasks());
+  //   })
+  // }
+
+  
 }
